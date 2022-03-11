e5eeaad0a55b3e1b0b98fb779e8a432884ddfef6
Skelebrinium <34858747+Skelebrinium@users.noreply.github.com>
2022-03-11 15:49:57 +0000

    main.py file

---
+++ main.py
@@ -0,0 +1,113 @@
+from sys import stdout
+from time import sleep
+from random import randint, uniform
+ 
+bag_contents = ["Beginner's Sword\U0001F5E1","Adventurer's Armour\u26E8","Beginner's Shield\U0001F6E1"]
+player_hp = 10
+exitprompt = False
+trolls_killed = 0
+ogres_escaped = 0
+giants_escaped = 0
+times_injured = 0
+
+def typewrite(text):
+  for i in text:
+    stdout.write(i)
+    stdout.flush()
+    # random float number between 0.055 and 0.085
+    # to make the typewriter look more human
+    sleep(round(uniform(0.055, 0.095), 3))
+ 
+typewrite("The Mystic Forest\n")
+while True:
+    # if players hp is 0 or less, they die
+    if player_hp <= 0:
+        typewrite("You have died.\n")
+        typewrite("Your statistics:\n")
+        print("Trolls killed: " + str(trolls_killed) + "\n" + "Ogres escaped: " + str(ogres_escaped) + "\n" + "Giants escaped: " + str(giants_escaped) + "\n" + "Times injured: " + str(times_injured))
+        quit()
+    option = str(input())
+    if option == "0":
+        typewrite("Your statistics:\n")
+        print("Trolls killed: " + str(trolls_killed) + "\n" + "Ogres escaped: " + str(ogres_escaped) + "\n" + "Giants escaped: " + str(giants_escaped) + "\n" + "Times injured: " + str(times_injured))
+        quit()
+    elif option == "1":
+        typewrite("Bag contents:\n")
+        if len(bag_contents) == 0:
+            typewrite("Your bag is empty.\n")
+        else:
+            for item in bag_contents:
+                print(item)
+    elif option == "2":
+        dice_roll = randint(1,6)
+        typewrite("You rolled a " + str(dice_roll) + ".\n")
+        if dice_roll == 1:
+            typewrite("You are fighting a troll.\n")
+            # if player has sword, kill the troll and break the sword. if player doesn't have sword, lose 2 hp.
+            if "Beginner's Sword\U0001F5E1" in bag_contents:
+                typewrite("You killed the troll!\n")
+                trolls_killed += 1
+                bag_contents.remove("Beginner's Sword\U0001F5E1")
+                typewrite("You broke the sword!\n")
+            else:
+                times_injured += 1
+                player_hp -= 2
+                typewrite("You lost 2 hp. Your hp is now " + str(player_hp) + ".\n")
+        elif dice_roll == 2:
+            typewrite("You are fighting an ogre.\n")
+            # if player has shield, escape the ogre and lose the shield. if player doesn't have shield, lose 3 hp.
+            if "Beginner's Shield\U0001F6E1" in bag_contents:
+                typewrite("You escaped the ogre!\n")
+                ogres_escaped += 1
+                bag_contents.remove("Beginner's Shield\U0001F6E1")
+                typewrite("You lost the shield!\n")
+            else:
+                times_injured += 1
+                player_hp -= 3
+                typewrite("You lost 3 hp. Your hp is now " + str(player_hp) + ".\n")
+        elif dice_roll == 3:
+            typewrite("You are fighting a giant.\n")
+            # if player has armour, escape the giant and lose the armour. if player doesn't have armour, lose 4 hp.
+            if "Adventurer's Armour\u26E8" in bag_contents:
+                typewrite("You escaped the giant!\n")
+                giants_escaped += 1
+                bag_contents.remove("Adventurer's Armour\u26E8")
+                typewrite("You lost the armour!\n")
+            else:
+                times_injured += 1
+                player_hp -= 4
+                typewrite("You lost 4 hp. Your hp is now " + str(player_hp) + ".\n")
+        elif dice_roll == 4:
+            typewrite("You found a thief!\n")
+            # if player has items in bag, thief steals first item
+            if len(bag_contents) > 0:
+                typewrite("The thief stole " + "'" + bag_contents[0] + "'" + "!" + "\n")
+                bag_contents.remove(bag_contents[0])
+            else:
+                typewrite("The thief stole nothing!\n")
+        elif dice_roll == 5:
+            typewrite("You found a healer!\n")
+            # if player hp is 7 or less, add 3. if player hp is 8, add 2. if player hp is 9, add 1.
+            if player_hp <= 7:
+                player_hp += 3
+                typewrite("You gained 3 hp. Your hp is now " + str(player_hp) + ".\n")
+            elif player_hp == 8:
+                player_hp += 2
+                typewrite("You gained 2 hp. Your hp is now " + str(player_hp) + ".\n")
+            elif player_hp == 9:
+                player_hp += 1
+                typewrite("You gained 1 hp. Your hp is now " + str(player_hp) + ".\n")
+            else:
+                typewrite("You already have 10 hp.\n")
+        elif dice_roll == 6:
+            # random item 1-3 is added to bag
+            random_num = randint(1,3)
+            if random_num == 1:
+                typewrite("You found a sword!\n")
+                bag_contents.append("Beginner's Sword\U0001F5E1")
+            elif random_num == 2:
+                typewrite("You found armor!\n")
+                bag_contents.append("Adventurer's Armour\u26E8")
+            elif random_num == 3:
+                typewrite("You found a shield!\n")
+                bag_contents.append("Beginner's Shield\U0001F6E1")
